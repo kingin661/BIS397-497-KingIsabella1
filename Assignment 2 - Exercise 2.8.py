e99a43a8663d03f77eea7452a3a8e884f4e0cee3
Bella <kingin.ink@gmail.com>
2020-05-26 19:48:15 -0400

    // Assignment 2 - Exercise 2.8.py

--- Assignment 2 - Exercise 2.8.py
+++ Assignment 2 - Exercise 2.8.py
@@ -1,11 +1,11 @@
 #Assignment 2 Exercise 2.8
-print('number', 'square' , 'cube')
-print(0,'\t','\t',0 ** 2,'\t',0 **3)
-print(1,'\t','\t',1 ** 2,'\t',1 **3)
-print(2,'\t','\t',2 ** 2,'\t',2 **3)
-print(3,'\t','\t',3 ** 2,'\t',3 **3)
-print(4,'\t','\t',4 ** 2,'\t',4 **3)
-print(5,'\t','\t',5 ** 2,'\t',5 **3)
+print('number','square','cube')
+print(0,'    ',0 ** 2,'    ',0**3)
+print(1,'    ',1 ** 2,'    ',1**3)
+print(2,'    ',2 ** 2,'    ',2**3)
+print(3,'    ',3 ** 2,'    ',3**3)
+print(4,'    ',4 ** 2,'   ',4**3)
+print(5,'    ',5 ** 2,'   ',5**3)
 #Challenge
 print('number', 'square' , 'cube')
 print(f'{0:>6.0f}{0 ** 2:>7.0f}{0 **3:>5.0f}')

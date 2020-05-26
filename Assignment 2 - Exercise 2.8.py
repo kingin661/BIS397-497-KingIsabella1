af449c00db01ec60248c4f7836a645f7817d7173
Bella <kingin.ink@gmail.com>
2020-05-26 17:25:55 -0400

    // Assignment 2 - Exercise 2.8.py

---
+++ Assignment 2 - Exercise 2.8.py
@@ -0,0 +1,9 @@
+#Assignment 2 Exercise 2.8
+print('number', 'square' , 'cube')
+print(0,'\t\t',0 ** 2,'\t\t' ,0 **3)
+print(1,'\t\t',1 ** 2,'\t\t' ,1 **3)
+print(2,'\t\t',2 ** 2,'\t\t' ,2 **3)
+print(3,'\t\t',3 ** 2,'\t\t' ,3 **3)
+print(4,'\t\t',4 ** 2,'\t' ,4 **3)
+print(5,'\t\t',5 ** 2,'\t' ,5 **3)
+

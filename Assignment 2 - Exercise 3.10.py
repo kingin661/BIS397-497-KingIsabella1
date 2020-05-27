f0c1c10cffa75c3e3d336bc8e068aaa9647dd48b
Bella <kingin.ink@gmail.com>
2020-05-26 23:14:19 -0400

    // Assignment 3.10

---
+++ Assignment 2 - Exercise 3.10.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 26 23:02:49 2020
+
+@author: Bella
+"""
+for number in range (1,31,1):
+    print(f'The balance at the end of year {number} is\
+ ${1000*(1.07)**number:.2f}')

b310481671f24b908f08220cda11dca7a2ca59c8
Bella <kingin.ink@gmail.com>
2020-05-28 13:18:08 -0400

    changed assignment 4.9

--- Assignment 2 - Exercise 4.9.py
+++ Assignment 2 - Exercise 4.9.py
@@ -6,8 +6,8 @@ Created on Wed May 27 22:03:14 2020
 """
 #Assigment 2 Exercise 4.9 
 
-print('Celsious    Fahrenheit')
+print('Celsius    Fahrenheit')
 
 for number in range(0,101,1):
     fahrenheit = ((9/5) * number +32)
-    print(f'{number:>5} {fahrenheit:>12.0f}')
+    print(f'{number:>4} {fahrenheit:>12.0f}')
